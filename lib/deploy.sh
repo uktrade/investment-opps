f789e7a6b7bdc1a4107f0297aa8a4be0908d39ec
breneser <brhneser@gmail.com>
2016-10-30 14:08:01 +0000

    Deploy script fixes and norobots text

--- lib/deploy.sh
+++ lib/deploy.sh
@@ -14,7 +14,22 @@ fi
 
 function deploy {
 echo "Deploying build to S3 bucket $BUCKET"
-aws s3 sync $PWD/build s3://"$BUCKET" --region="$REGION" --delete --storage-class REDUCED_REDUNDANCY
+
+#directory of this script
+
+if [ "$NO_ROBOTS" = "true" ]; then
+	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	echo "No robots flag set for this deployment, deploying no robots configuration"
+	cp "$dir"/norobots.txt "$PWD"/build/robots.txt 
+fi
+
+aws s3 sync "$PWD"/build s3://"$BUCKET" --region="$REGION" --delete --storage-class REDUCED_REDUNDANCY
+
+#Clean no robots text file
+if [ "$NO_ROBOTS" = "true" ]; then
+	rm "$PWD"/build/robots.txt
+fi	
+
 }
 
 verify && deploy
