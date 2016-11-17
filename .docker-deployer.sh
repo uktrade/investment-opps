158adb6c51a5d53e9b6fa1419ede3969b4d1b3f6
breneser <brhneser@gmail.com>
2016-11-17 13:38:02 +0000

    Added docker deployer script

---
+++ .docker-deployer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+[ -z "$DOCKER_PATH" ] && echo "DOCKER_PATH is not set, exiting..." && exit 1
+[ -z "$DOCKER_IMAGE_NAME" ] && echo "DOCKER_IMAGE_NAME is not set, using name 'iigb-deployer' as image name" && \
+     DOCKER_IMAGE_NAME=iigb
+
+
+DOCKER_IMAGE="$DOCKER_IMAGE_NAME-deployer"
+
+$DOCKER_PATH build --force-rm -t $DOCKER_IMAGE $PWD  && \
+$DOCKER_PATH run --rm -i --name $DOCKER_IMAGE  -e BUCKET=$BUCKET -e DID=$DID -e AWS_ACCESS_KEY_ID=$ -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e NO_ROBOTS=$NO_ROBOTS  $DOCKER_IMAGE /bin/bash -c "npm run deploy"
+
+
+result=$?
+
+$DOCKER_PATH rmi --force $DOCKER_IMAGE
+
+if [ $result -ne 0 ]; then
+     echo "Deployment failed!! See above for error details"
+     exit 1
+fi
