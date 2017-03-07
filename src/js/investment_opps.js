76fb83e85b30002d4682cf8326d240efa75611d8
breneser <brhneser@gmail.com>
2017-03-07 18:06:43 +0000

    Add filters and tabs

--- src/js/investment_opps.js
+++ src/js/investment_opps.js
@@ -9,8 +9,9 @@ var _assets=document.iigbBuild ? '/assets/' + document.iigbBuild + '/':'/assets/
 var TAFFY=require('taffydb').taffy
 var table
 var data
-var filter
-var industry
+var filter={}
+var filteredData
+var currentTab='tab-map'
 
 function init() {
   var oppsContainer=$('#investment-opps-container')
@@ -22,26 +23,108 @@ function init() {
     .done(function(list) {
       map.init(oppsContainer.find('.map-view'))
       data=TAFFY(list)
-      // table=$('#opps-table')
-      // table.bootstrapTable({data: []})
+      table=$('#opps-table')
+      table.bootstrapTable({data: []})
       watch()
     })
     .fail(fail)
 }
-
 function watch() {
+  initTabs()
+  watchIndustryFilter()
+  watchBusinessesFilter()
+  watchCentresFilter()
+}
+
+function watchIndustryFilter() {
   $('.industry-selector')
     .change(function() {
       var selected=$(this).val()
-      if(selected === industry) {
+      if(selected === filter.industry) {
         return
       }
-      industry=selected
-      filter=data({industry: selected})
+      filter.industry=selected
       debug('Selected industry', selected)
-      // refreshTable(filter.get())
-      map.refresh(filter.get())
+      filterData()
     })
+}
+
+function watchBusinessesFilter() {
+  $('#significant-businesses')
+    .change(function() {
+      var checked= $(this).is(':checked')
+      filter.businesses= checked
+      filterData()
+    })
+}
+
+function watchCentresFilter() {
+  $('#innovation-centres')
+    .change(function() {
+      var checked= $(this).is(':checked')
+      filter.centres= checked
+      filterData()
+    })
+}
+
+function initTabs() {
+  var controls=$('[data-tab-control]')
+  var tabs=[]
+  controls.each(function() {
+    tabs.push($('#'+ $(this).data('tab-control')))
+  })
+
+  controls
+    .click(function() {
+      var goto=$(this).data('tab-control')
+      if(currentTab===goto) {
+        return
+      }
+      debug('Changing tab to ', goto)
+      changeTab(goto)
+      render()
+    })
+
+
+  function changeTab(goto) {
+    currentTab=goto
+    controls.each(function(index) {
+      var control=$(this)
+      var tab=tabs[index]
+      if(control.data('tab-control')===goto)  {
+        control.addClass('active')
+        tab.addClass('in active')
+      }else {
+        control.removeClass('active')
+        tab.removeClass('in active')
+      }
+
+    })
+  }
+}
+
+function filterData() {
+  debug('Filtering data by', filter)
+  var _filter= {}
+  if(filter.industry) {
+    _filter.industry=filter.industry
+  }
+  if(filter.businesses) {
+    _filter.businesses = {gt: 0}
+  }
+  if(filter.centres) {
+    _filter.centres = {gt: 0}
+  }
+  filteredData=data(_filter).get()
+  render()
+}
+
+function render() {
+  if(currentTab === 'tab-table') {
+    refreshTable(filteredData)
+  } else if (currentTab==='tab-map') {
+    map.refresh(filteredData)
+  }
 }
 
 function refreshTable(list) {
