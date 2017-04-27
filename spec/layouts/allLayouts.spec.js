f4fef3ba4a46398ed7f8fb8d75837a025c590f93
breneser <brhneser@gmail.com>
2017-04-27 14:48:34 +0100

    Fix wrong cms tags in new form template

--- spec/layouts/allLayouts.spec.js
+++ spec/layouts/allLayouts.spec.js
@@ -1,51 +1,48 @@
-/* jshint ignore:start*/
-var path=require('path');
-describe('All layouts tags', function(){
+var path = require('path')
+describe('All layouts tags', function() {
   loopLayouts(function(file) {
     describe(path.basename(file) + ' cms tags', function() {
-        var html = render(file);
-        var fields = parseCmsTags(html);
-        var pageHeader = fields['pageHeader'];
-        it('should define pageHeader',function() {
-          expect(pageHeader).toBeDefined();
-        });
+      var html = render(file)
+      var fields = parseCmsTags(html)
+      var pageHeader = fields['pageHeader']
+      it('should define pageHeader', function() {
+        expect(pageHeader).toBeDefined()
+      })
 
-        it('pageHeader should be of type content with label',function() {
-          expect(pageHeader.type).toBe('content');
-        });
+      it('pageHeader should be of type content with label', function() {
+        expect(pageHeader.type).toBe('content')
+      })
 
-        it('pageHeader should have optional heroImage field definition with a label',function() {
-          var heroImage = pageHeader.fields.heroImage;
-          expect(heroImage).toBeDefined();
-          expect(heroImage.type).toBe('text');
-          expect(heroImage.label).toBeDefined();
-          expect(heroImage.required).not.toBe(true);
-        });
+      it('pageHeader should have optional heroImage field definition with a label', function() {
+        var heroImage = pageHeader.fields.heroImage
+        expect(heroImage).toBeDefined()
+        expect(heroImage.type).toBe('text')
+        expect(heroImage.label).toBeDefined()
+        expect(heroImage.required).not.toBe(true)
+      })
 
-        it('pageHeader should have optional heroVideo field definition with a label',function() {
-          var heroVideo = pageHeader.fields.heroVideo;
-          expect(heroVideo).toBeDefined();
-          expect(heroVideo.type).toBe('text');
-          expect(heroVideo.required).not.toBe(true);
-        });
+      it('pageHeader should have optional heroVideo field definition with a label', function() {
+        var heroVideo = pageHeader.fields.heroVideo
+        expect(heroVideo).toBeDefined()
+        expect(heroVideo.type).toBe('text')
+        expect(heroVideo.required).not.toBe(true)
+      })
 
-        it('pageHeader should have optional externalVideo field definition with a label',function() {
-          var field = pageHeader.fields.externalVideo;
-          expect(field).toBeDefined();
-          expect(field.type).toBe('text');
-          expect(field.label).toBeDefined();
-          expect(field.required).not.toBe(true);
-        });
+      it('pageHeader should have optional externalVideo field definition with a label', function() {
+        var field = pageHeader.fields.externalVideo
+        expect(field).toBeDefined()
+        expect(field.type).toBe('text')
+        expect(field.label).toBeDefined()
+        expect(field.required).not.toBe(true)
+      })
 
-        it('pageHeader should have optional heroVideoImage field definition with a label',function() {
-          var field = pageHeader.fields.heroVideoImage;
-          expect(field).toBeDefined();
-          expect(field.type).toBe('text');
-          expect(field.label).toBeDefined();
-          expect(field.required).not.toBe(true);
-        });
-    });
-  });
-});
-/* jshint ignore:end*/
-
+      it('pageHeader should have optional heroVideoImage field definition with a label', function() {
+        var field = pageHeader.fields.heroVideoImage
+        expect(field).toBeDefined()
+        expect(field.type).toBe('text')
+        expect(field.label).toBeDefined()
+        expect(field.required).not.toBe(true)
+      })
+    })
+  })
+})
